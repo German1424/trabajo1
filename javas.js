57e6de7e326d1a6037f0fb2145dd743caf6769bf
German1424 <143235340+German1424@users.noreply.github.com>
2023-10-28 08:27:49 -0600

    Add files via upload
    codigo de Java Script

---
+++ javas.js
@@ -0,0 +1,18 @@
+alert ("hola")
+var usuario = "german";
+var pass = "1234";
+var user = prompt("Escribi tu usuario ");
+var pass = prompt("ingrese su contraseña")
+if (user == usuario) {
+    alert ("Tu usuario es correcto");
+} else {
+    alert (" tu usuario es incorrecto");
+}
+if (pass == pass) {
+    alert ("contraseña correcta");
+} else {
+    alert (" contraseña ncorrecta");
+}
+
+
+
